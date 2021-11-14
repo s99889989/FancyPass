9d4711acec5d9f3236a0a83b97807c08cc00fe4f
Daxton <s02048898067@gmail.com>
2021-11-14 13:12:38 +0800

    1

---
+++ src/main/java/com/daxton/fancypass/FancyPass.java
@@ -0,0 +1,17 @@
+package com.daxton.fancypass;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyPass extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
